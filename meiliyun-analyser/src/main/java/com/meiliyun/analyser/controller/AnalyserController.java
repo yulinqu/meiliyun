b938dd1fd1bad7bdf9a4b411aeeb1604aa1d60b0
yulinqu <yulinqu@mobanker.com>
2017-10-12 18:44:32 +0800

    test

--- meiliyun-analyser/src/main/java/com/meiliyun/analyser/controller/AnalyserController.java
+++ meiliyun-analyser/src/main/java/com/meiliyun/analyser/controller/AnalyserController.java
@@ -1,5 +1,7 @@
 package com.meiliyun.analyser.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,20 +16,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AnalyserController {
 
-    /**
-     * 测试接口
-     * 
-     * @return
-     */
-    @RequestMapping(value = "heartbeat_test", method = RequestMethod.GET)
-    public @ResponseBody String heartbeatTest() {
-
-        return "1";
-
-    }
-
     @RequestMapping(value = "analyser", method = RequestMethod.GET)
-    public @ResponseBody String analyser(@RequestParam("ad") String ad, @RequestParam("freq") String freq) {
+    public @ResponseBody String analyser(@RequestParam(value = "ad", required = false) String ad,
+            @RequestParam("staticTimeUnit") String staticTimeUnit, @RequestParam("startTime") String startTime,
+            @RequestParam("endTime") String endTime, @RequestParam("url") String url) {
+
+        /**
+         * 1.根据时间获取该时间维度的文件 2.遍历这些文件 解析 每一个文件 每一行的数据 3.根据staticTimeUnit 去service 里面 调用不同的方法
+         */
+
+        File scpFile = new File("");
 
         return "1";
 
